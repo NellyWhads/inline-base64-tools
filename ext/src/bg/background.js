06174482bfe6ec4e0a2b01ee6ca95b3cb0dbe604
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2020-11-13 10:23:52 +0700

    Add open on new tab option

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,37 +1,41 @@
 function sendMessage(message) {
-    chrome.tabs.query({
-        "active": true,
-        "currentWindow": true
-    }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, message);
-    });
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, message);
+    }
+  );
 }
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    // Logging to background extension console
-    console.log("item " + info.menuItemId + " was clicked");
+  // Logging to background extension console
+  console.log("item " + info.menuItemId + " was clicked");
 
-    // Handle checked state toggling
-    if (info.menuItemId === "copyToClipboard") {
-        chrome.storage.local.set({ "copyToClipboard": info.checked });
-        return;
-    }
-    else if (info.menuItemId === "decodeOnSelect") {
-        chrome.storage.local.set({ "decodeOnSelect": info.checked });
-        return;
-    }
+  // Handle checked state toggling
+  if (info.menuItemId === "copyToClipboard") {
+    chrome.storage.local.set({ copyToClipboard: info.checked });
+    return;
+  } else if (info.menuItemId === "decodeOnSelect") {
+    chrome.storage.local.set({ decodeOnSelect: info.checked });
+    return;
+  } else if (info.menuItemId === "openOnNewTab") {
+    chrome.storage.local.set({ openOnNewTab: info.checked });
+    return;
+  }
 
-    // Select method
-    var message = ""
-    if (info.menuItemId === "decodeBase64") {
-        message = { "invoke": "decodeBase64Selection"};
-    }
-    else if (info.menuItemId === "encodeBase64") {
-        message = { "invoke": "encodeBase64Selection"};
-    }
+  // Select method
+  var message = "";
+  if (info.menuItemId === "decodeBase64") {
+    message = { invoke: "decodeBase64Selection" };
+  } else if (info.menuItemId === "encodeBase64") {
+    message = { invoke: "encodeBase64Selection" };
+  }
 
-    // Send message
-    if (message) {
-        sendMessage(message);
-    }
+  // Send message
+  if (message) {
+    sendMessage(message);
+  }
 });
--- ext/src/bg/menu.js
+++ ext/src/bg/menu.js
@@ -1,41 +1,59 @@
 chrome.runtime.onInstalled.addListener(function () {
-    const parentGroupID = chrome.contextMenus.create({ // Parent Group
-        "id": "parentGroup",
-        "title": chrome.i18n.getMessage("extensionName"),
-        "contexts": ["selection"],
-    });
+  const parentGroupID = chrome.contextMenus.create({
+    // Parent Group
+    id: "parentGroup",
+    title: chrome.i18n.getMessage("extensionName"),
+    contexts: ["selection"],
+  });
 
-    const decodeID = chrome.contextMenus.create({ // Base64 Decode Selection
-        "id": "decodeBase64",
-        "parentId": parentGroupID,
-        "title": chrome.i18n.getMessage("decode"),
-        "contexts": ["selection"],
-    });
+  const decodeID = chrome.contextMenus.create({
+    // Base64 Decode Selection
+    id: "decodeBase64",
+    parentId: parentGroupID,
+    title: chrome.i18n.getMessage("decode"),
+    contexts: ["selection"],
+  });
 
-    const encodeID = chrome.contextMenus.create({ // Base64 Encode Selection
-        "id": "encodeBase64",
-        "parentId": parentGroupID,
-        "title": chrome.i18n.getMessage("encode"),
-        "contexts": ["selection"],
-    });
+  const encodeID = chrome.contextMenus.create({
+    // Base64 Encode Selection
+    id: "encodeBase64",
+    parentId: parentGroupID,
+    title: chrome.i18n.getMessage("encode"),
+    contexts: ["selection"],
+  });
 
-    chrome.storage.local.get(["copyToClipboard", "decodeOnSelect"], function (result) {
-        const copyID = chrome.contextMenus.create({ // Copy resulting string to clipboard
-            "id": "copyToClipboard",
-            "parentId": parentGroupID,
-            "type": "checkbox",
-            "checked": result.copyToClipboard,
-            "title": chrome.i18n.getMessage("copyToClipboard"),
-            "contexts": ["selection"],
-        });
+  chrome.storage.local.get(
+    ["copyToClipboard", "decodeOnSelect", "openOnNewTab"],
+    function (result) {
+      const copyID = chrome.contextMenus.create({
+        // Copy resulting string to clipboard
+        id: "copyToClipboard",
+        parentId: parentGroupID,
+        type: "checkbox",
+        checked: result.copyToClipboard,
+        title: chrome.i18n.getMessage("copyToClipboard"),
+        contexts: ["selection"],
+      });
 
-        const decodeOnSelectID = chrome.contextMenus.create({ // Copy resulting string to clipboard
-            "id": "decodeOnSelect",
-            "parentId": parentGroupID,
-            "type": "checkbox",
-            "checked": result.decodeOnSelect,
-            "title": chrome.i18n.getMessage("decodeOnSelect"),
-            "contexts": ["selection"],
-        });
-	});
+      const decodeOnSelectID = chrome.contextMenus.create({
+        // Copy resulting string to clipboard
+        id: "decodeOnSelect",
+        parentId: parentGroupID,
+        type: "checkbox",
+        checked: result.decodeOnSelect,
+        title: chrome.i18n.getMessage("decodeOnSelect"),
+        contexts: ["selection"],
+      });
+
+      const openOnNewTab = chrome.contextMenus.create({
+        //   Open link on new tab
+        id: "openOnNewTab",
+        parentId: parentGroupID,
+        type: "checkbox",
+        checked: result.openOnNewTab,
+        title: chrome.i18n.getMessage("openOnNewTab"),
+        contexts: ["selection"],
+      });
+    }
+  );
 });
